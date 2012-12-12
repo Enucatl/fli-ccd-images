eb13f46ea6e78704ac09be73eab344b044155690
Matteo Abis <gmatteo.abis@gmail.com>
2012-12-12 15:21:40 +0100

    added skimage reader

---
+++ skimage_reader.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+from __future__ import division, print_function
+import numpy
+from skimage import io, transform
+
+def header(file_name):
+    lines = []
+    rows = 0
+    columns = 0
+    with open(file_name, "rb") as input_file:
+        for line in input_file:
+            lines.append(line)
+            if "rows" in line:
+                rows = int(line.split()[2])
+            elif "columns" in line:
+                columns = int(line.split()[2])
+            if "EOH" in line:
+                break
+    return rows, columns, len("".join(lines)) + 1
+
+def fromfile(file_name):
+    "similar to numpy.fromfile, cuts the header and sets the image"
+    rows, columns, header_len = header(file_name)
+    with open(file_name) as input_file:
+        input_file.read(header_len)
+        array = numpy.fromfile(input_file, dtype=numpy.uint16) 
+    array = numpy.resize(array, (rows, columns))
+    return transform.rotate(io.Image(array), 90)
+
+if __name__ == '__main__':
+    import sys
+    image = fromfile(sys.argv[1])
+    io.imshow(image)
+    io.show()
