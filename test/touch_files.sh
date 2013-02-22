b6fd8599c3d93b656f8e04480bc824baa29c8a17
Matteo Abis <gmatteo.abis@gmail.com>
2013-02-22 13:59:22 +0100

    added touch files

---
+++ test/touch_files.sh
@@ -0,0 +1,9 @@
+while true
+do
+    echo "touching first..."
+    touch test/ccdimage_ct_000547.raw
+    sleep 0.5
+    echo "touching second..."
+    touch test/ccdimage_ct_000754.raw
+    sleep 0.5
+done
