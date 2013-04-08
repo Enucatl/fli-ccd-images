1d9c012c9be8051632b665dfbcd4683a9cd11c05
specuser <specuser.mpc1054@gmail.com>
2013-04-08 11:09:05 +0200

    default projection.sh to pixel 508

--- bash/projection_stack.sh
+++ bash/projection_stack.sh
@@ -3,4 +3,4 @@ echo "projection stack on pixel 515 from file"
 root_file=$(./bash/scan_folder.sh $1).root
 echo $root_file
 ./bash/make_root.sh $1
-./python/projection_stack.py $root_file 515
+./python/projection_stack.py $root_file 508
