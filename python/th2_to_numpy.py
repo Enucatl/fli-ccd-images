67f204d7ca4f03eeab3c7fba03be2e43a6303438
Matteo Abis <gmatteo.abis@gmail.com>
2013-04-09 15:40:39 +0200

    added th2 to numpy function

---
+++ python/th2_to_numpy.py
@@ -0,0 +1,20 @@
+def th2_to_numpy(histogram):
+    """Convert TH2 histogram to numpy array
+
+    :histogram: TH2 object
+    :returns: numpy array
+
+    """
+    width = histogram.GetNbinsX()
+    height = histogram.GetNbinsY()
+    image = np.fromiter(
+            (histogram.fArray[i]
+                for i in range(histogram.fN)),
+            dtype=np.uint16,
+            count=histogram.fN)
+    image_array = img_as_uint(image)
+    image_array = np.reshape(image_array, (height + 2, width + 2))
+    image_array = np.delete(image_array, (0, height + 1), 0)
+    image_array = np.delete(image_array, (0, width + 1), 1)
+    image_array = np.flipud(image_array)
+    return image_array
