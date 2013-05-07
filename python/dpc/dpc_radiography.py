dad39a17471a7f37b31da5e5bd698bb404fc01b9
Matteo Abis <gmatteo.abis@gmail.com>
2013-05-07 14:52:15 +0200

    dpc projection as class works

--- python/dpc/dpc_radiography.py
+++ python/dpc/dpc_radiography.py
@@ -18,81 +18,100 @@ from dpc.phase_stepping_utils import correct_drift
 from dpc.commandline_parser import commandline_parser
 from projections.handle_projection_stack import get_projection_stack
 
-commandline_parser.description = __doc__
+class ImageReconstructor(object):
+    """Reconstruct the three signals from the phase stepping curve of the
+    grating interferometer.
+    
+    """
+
+    def __init__(self, args):
+        root_file, histogram = get_projection_stack(args)
+        args.file = args.flat
+        flat_root_file, flat_histogram = get_projection_stack(args)
+        image_array = th2_to_numpy(histogram, args.roi)
+        flat_image = th2_to_numpy(flat_histogram, args.roi)
+        self.n_steps = args.steps[0]
+        self.n_lines = image_array.shape[0] // self.n_steps 
+        if image_array.shape[0] % self.n_steps:
+            raise ValueError("""wrong number of steps,
+            division does not result in an integer.
+            Image shape: {0}""".format(image_array.shape))
+        self.extension = args.format[0]
+        self.n_periods = args.periods
+        self.flat_parameters = get_signals(flat_image, n_periods=self.n_periods)
+        self.images = np.split(image_array, self.n_lines, axis=0)
+        self.absorption_image = np.zeros((self.n_lines, image_array.shape[1]))
+        self.differential_phase_image = np.zeros_like(self.absorption_image)
+        self.dark_field_image = np.zeros_like(self.absorption_image)
+
+    def calculate_images(self):
+        for i, phase_stepping_curve in enumerate(self.images):
+            absorption, phase, dark_field = get_signals(
+                    phase_stepping_curve,
+                    self.flat_parameters,
+                    self.n_periods)
+            self.absorption_image[i, :] = absorption
+            self.differential_phase_image[i, :] = phase
+            self.dark_field_image[i, :] = dark_field
+
+    def draw(self, file=""):
+        f, (ax1, ax2, ax3) = plt.subplots(
+                3, 1, sharex=True)
+        img1 = ax1.imshow(self.absorption_image,
+                cmap=plt.cm.Greys)
+        ax1.axis("off")
+        ax1.set_title("absorption")
+        img2 = ax2.imshow(self.differential_phase_image)
+        img2.set_clim(0, 1.5)
+        ax2.axis("off")
+        ax2.set_title("differential phase")
+        img3 = ax3.imshow(self.dark_field_image)
+        ax3.set_title("visibility reduction")
+        ax3.axis("off")
+        img3.set_clim(0, 2)
+        plt.tight_layout()
+        if self.absorption_image.shape[0] == 1:
+            f, (hist1, hist2, hist3) = plt.subplots(
+                    3, 1, sharex=True)
+            hist1.hist(range(self.absorption_image.shape[1]),
+                    bins=self.absorption_image.shape[1],
+                    weights=self.absorption_image.T, fc='w', ec='k')
+            hist1.set_title("absorption")
+            hist2.hist(range(self.differential_phase_image.shape[1]),
+                    bins=self.differential_phase_image.shape[1],
+                    weights=self.differential_phase_image.T, fc='w', ec='k')
+            hist2.set_title("differential phase")
+            hist3.hist(range(self.dark_field_image.shape[1]),
+                    bins=self.dark_field_image.shape[1],
+                    weights=self.dark_field_image.T, fc='w', ec='k')
+            hist3.set_title("visibility reduction")
+        #plt.figure()
+        #plt.hist(image_array.flatten(), 256,
+                #range=(np.amin(image_array),
+                    #np.amax(image_array)), fc='w', ec='k')
+        #plt.figure()
+        #plt.hist(self.dark_field_image.flatten(), 256,
+                #range=(np.amin(self.dark_field_image),
+                    #np.amax(self.dark_field_image)), fc='k', ec='k')
+        #plt.figure()
+        #plt.hist(self.differential_phase_image.flatten(), 256,
+                #range=(np.amin(self.differential_phase_image),
+                    #np.amax(self.differential_phase_image)), fc='k', ec='k')
+        #print("mean phase {0:.4f} +- {1:.4f}".format(
+                #np.mean(self.differential_phase_image),
+                #np.std(self.differential_phase_image) /
+                #math.sqrt(roi[1] - roi[0])))
+        #plt.savefig(root_file.GetName().replace(".root",
+            #"." + extension))
+        if not file:
+            plt.show()
+        else:
+            plt.savefig(file)
+        
+commandline_parser.description = ImageReconstructor.__doc__
 
 if __name__ == '__main__':
     args = commandline_parser.parse_args()
-    root_file, histogram = get_projection_stack(args)
-    roi = args.roi
-    args.file = args.flat
-    flat_root_file, flat_histogram = get_projection_stack(args)
-    image_array = th2_to_numpy(histogram, roi)
-    flat_image = th2_to_numpy(flat_histogram, roi)
-    n_steps = args.steps[0]
-    n_lines = image_array.shape[0] // n_steps 
-    extension = args.format[0]
-    n_periods = args.periods
-    flat_parameters = get_signals(flat_image, n_periods=n_periods)
-    images = np.split(image_array, n_lines, axis=0)
-    absorption_image = np.zeros((n_lines, image_array.shape[1]))
-    differential_phase_image = np.zeros_like(absorption_image)
-    dark_field_image = np.zeros_like(absorption_image)
-    for i, phase_stepping_curve in enumerate(images):
-        absorption, phase, dark_field = get_signals(
-                phase_stepping_curve,
-                flat_parameters,
-                n_periods)
-        absorption_image[i, :] = absorption
-        differential_phase_image[i, :] = phase
-        dark_field_image[i, :] = dark_field
-
-    drift_corrected = correct_drift(differential_phase_image)
-    f, (ax1, ax2, ax3) = plt.subplots(
-            3, 1, sharex=True)
-    img1 = ax1.imshow(absorption_image,
-            cmap=plt.cm.Greys)
-    ax1.axis("off")
-    ax1.set_title("absorption")
-    img2 = ax2.imshow(differential_phase_image)
-    img2.set_clim(0, 1.5)
-    ax2.axis("off")
-    ax2.set_title("differential phase")
-    img3 = ax3.imshow(dark_field_image)
-    ax3.set_title("visibility reduction")
-    ax3.axis("off")
-    img3.set_clim(0, 2)
-    plt.tight_layout()
-    if absorption_image.shape[0] == 1:
-        f, (hist1, hist2, hist3) = plt.subplots(
-                3, 1, sharex=True)
-        hist1.hist(range(absorption_image.shape[1]),
-                bins=absorption_image.shape[1],
-                weights=absorption_image.T, fc='w', ec='k')
-        hist1.set_title("absorption")
-        hist2.hist(range(differential_phase_image.shape[1]),
-                bins=differential_phase_image.shape[1],
-                weights=differential_phase_image.T, fc='w', ec='k')
-        hist2.set_title("differential phase")
-        hist3.hist(range(dark_field_image.shape[1]),
-                bins=dark_field_image.shape[1],
-                weights=dark_field_image.T, fc='w', ec='k')
-        hist3.set_title("visibility reduction")
-    #plt.figure()
-    #plt.hist(image_array.flatten(), 256,
-            #range=(np.amin(image_array),
-                #np.amax(image_array)), fc='w', ec='k')
-    #plt.figure()
-    #plt.hist(dark_field_image.flatten(), 256,
-            #range=(np.amin(dark_field_image),
-                #np.amax(dark_field_image)), fc='k', ec='k')
-    #plt.figure()
-    #plt.hist(differential_phase_image.flatten(), 256,
-            #range=(np.amin(differential_phase_image),
-                #np.amax(differential_phase_image)), fc='k', ec='k')
-    #print("mean phase {0:.4f} +- {1:.4f}".format(
-            #np.mean(differential_phase_image),
-            #np.std(differential_phase_image) /
-            #math.sqrt(roi[1] - roi[0])))
-    #plt.savefig(root_file.GetName().replace(".root",
-        #"." + extension))
-    plt.show()
+    ir = ImageReconstructor(args)
+    ir.calculate_images()
+    ir.draw()
