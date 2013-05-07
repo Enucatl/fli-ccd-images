8bfaf847bd00ea4c7741b4daa1fd52901cc7347a
Matteo Abis <gmatteo.abis@gmail.com>
2013-05-07 14:58:46 +0200

    added drift correction to the class

--- python/dpc/dpc_radiography.py
+++ python/dpc/dpc_radiography.py
@@ -10,11 +10,11 @@ ROOT.PyConfig.IgnoreCommandLineOptions = True
 
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy import stats
 
 from readimages_utils.th2_to_numpy import th2_to_numpy
 from dpc.phase_stepping_utils import average_curve
 from dpc.phase_stepping_utils import get_signals
-from dpc.phase_stepping_utils import correct_drift
 from dpc.commandline_parser import commandline_parser
 from projections.handle_projection_stack import get_projection_stack
 
@@ -53,6 +53,9 @@ class ImageReconstructor(object):
             self.absorption_image[i, :] = absorption
             self.differential_phase_image[i, :] = phase
             self.dark_field_image[i, :] = dark_field
+        self.absorption_image_title = "absorption image"
+        self.differential_phase_image_title = "differential phase"
+        self.dark_field_image_title = "visibility reduction"
 
     def draw(self, file=""):
         f, (ax1, ax2, ax3) = plt.subplots(
@@ -60,13 +63,13 @@ class ImageReconstructor(object):
         img1 = ax1.imshow(self.absorption_image,
                 cmap=plt.cm.Greys)
         ax1.axis("off")
-        ax1.set_title("absorption")
+        ax1.set_title(self.absorption_image_title)
         img2 = ax2.imshow(self.differential_phase_image)
-        img2.set_clim(0, 1.5)
+        img2.set_clim(-0.4, 0.4)
         ax2.axis("off")
-        ax2.set_title("differential phase")
+        ax2.set_title(self.differential_phase_image_title)
         img3 = ax3.imshow(self.dark_field_image)
-        ax3.set_title("visibility reduction")
+        ax3.set_title(self.dark_field_image_title)
         ax3.axis("off")
         img3.set_clim(0, 2)
         plt.tight_layout()
@@ -107,6 +110,30 @@ class ImageReconstructor(object):
             plt.show()
         else:
             plt.savefig(file)
+
+    def correct_drift(self, verbose=False):
+        """Fit a vertical line to the phase image in order to subtract a
+        linear phase drift.
+
+        """
+        x = np.arange(self.differential_phase_image.shape[0])
+        y = np.sum(self.differential_phase_image, axis=1) / self.differential_phase_image.shape[1]
+        slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
+        line = slope * x + intercept
+        correction = np.tile(line,
+                (self.differential_phase_image.shape[1], 1)).transpose()
+        self.differential_phase_image_title += " (drift corrected)"
+        self.differential_phase_image -= correction
+        if verbose:
+            plt.figure()
+            axis = plt.axes()
+            axis.set_title("phase drift interpolation")
+            plt.plot(x, line, 'r-', x, y, 'o')
+            plt.xlabel('image number')
+            plt.ylabel('average phase (rad)')
+            #plt.figure()
+            #image = plt.imshow(corrected_image)
+            #image.set_clim(-0.5, 0.5)
         
 commandline_parser.description = ImageReconstructor.__doc__
 
@@ -114,4 +141,5 @@ if __name__ == '__main__':
     args = commandline_parser.parse_args()
     ir = ImageReconstructor(args)
     ir.calculate_images()
+    ir.correct_drift()
     ir.draw()
