a166b9467e6a0d31261c2ddf826225198a1f91a1
Matteo Abis <gmatteo.abis@gmail.com>
2013-05-10 10:22:40 +0200

    hdf5 single image test working

--- test/test_hdf5.py
+++ test/test_hdf5.py
@@ -20,22 +20,30 @@ if __name__ == '__main__':
     image_name = os.path.splitext(os.path.basename(file_name))[0]
     header_lines = 16
     header = list(islice(input_file, header_lines))
-    print("".join(header))
     header_len = len("".join(header))
+    exposure_time = float(header[4].split()[-1])
     min_y, min_x, max_y, max_x = [int(x) for x in header[-2].split()[2:]]
     input_file.close()
     input_file = open(file_name, 'rb')
     input_file.read(header_len + 1)
-    array = np.fromfile(input_file, dtype=np.uint16)
-    array = np.reshape(array, (max_x - min_x, max_y - min_y)).T
-    print(array.shape)
-    print(array)
-    plt.figure()
-    plt.imshow(array)
-    dataset = group.create_dataset(image_name, data=array)
-    dataset.attrs['min_x'] = min_x
-    dataset.attrs['min_y'] = min_y
-    dataset.attrs['max_x'] = max_x
-    dataset.attrs['max_y'] = max_y
-    output_file.close()
-    plt.show()
+    image = np.reshape(np.fromfile(input_file, dtype=np.uint16),
+            (max_y - min_y, max_x - min_x),
+            order='FORTRAN')
+    show = True
+    if show:
+        print("".join(header))
+        print(image.shape)
+        print(image)
+        print("exposure", exposure_time)
+        plt.figure()
+        plt.imshow(image, origin='lower',
+                extent=[min_x, max_x, min_y, max_y])
+        plt.locator_params(axis='y', nbins=3)
+        dataset = group.create_dataset(image_name, data=image)
+        dataset.attrs['exposure_time'] = exposure_time
+        dataset.attrs['min_x'] = min_x
+        dataset.attrs['min_y'] = min_y
+        dataset.attrs['max_x'] = max_x
+        dataset.attrs['max_y'] = max_y
+        output_file.close()
+        plt.show()
