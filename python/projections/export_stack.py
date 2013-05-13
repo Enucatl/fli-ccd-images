93d48165d8add31368f4c8431e9aea700be52e11
Matteo Abis <gmatteo.abis@gmail.com>
2013-05-13 16:10:22 +0200

    export stack converted to hdf5

--- python/projections/export_stack.py
+++ python/projections/export_stack.py
@@ -4,22 +4,23 @@ from __future__ import division, print_function
 import os
 import argparse
 
-import ROOT
-ROOT.PyConfig.IgnoreCommandLineOptions = True
+import matplotlib.pyplot as plt
 
-from skimage import io
-
-from readimages_utils.th2_to_numpy import th2_to_numpy
-from projections.handle_projection_stack import get_projection_stack
+import readimages_utils.rcparams
+from projections.projection_stack import get_projection_stack
+from projections.commandline_parser import commandline_parser
+from readimages_utils.hadd import hadd
 
 """Export the projection stack to an image."""
 
 if __name__ == '__main__':
-    from projections.commandline_parser import commandline_parser
     args = commandline_parser.parse_args()
     extension = args.format[0]
-    root_file, histogram = get_projection_stack(args)
-    image_array = th2_to_numpy(histogram)
-    io.use_plugin("freeimage")
-    output_name = root_file_name.replace(".root", "." + extension)
-    io.imsave(output_name, image_array)
+    image_array = get_projection_stack(args.file, args)
+    plt.figure()
+    plt.imshow(image_array)
+    output_name = hadd(args.file).replace(".hdf5", "." + extension)
+    plt.imsave(output_name)
+    plt.ion()
+    plt.show()
+    raw_input("Press ENTER to quit.")
