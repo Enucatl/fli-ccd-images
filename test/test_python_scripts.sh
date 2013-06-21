c5e2fee90e38eb9913a50f503fe3c081c6dfd9d1
Matteo Abis <gmatteo.abis@gmail.com>
2013-06-21 09:21:53 +0200

    removed useless data from test

--- test/test_python_scripts.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm test.root; ./bin/make_root test; python python/convert_images.py test.root
-rm test.root; ./bin/make_root test; python python/intensity_scan.py test.root --roi 520 570 520 525
-rm test.root; ./bin/make_root test; python python/make_projection_stack.py test.root 520
