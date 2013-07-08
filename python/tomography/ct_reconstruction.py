5d1cfb48532691ff36e1b2bb08c5ba38cd483057
Matteo Abis <gmatteo.abis@gmail.com>
2013-07-08 17:16:32 +0200

    fixed ct_reconstruction not displaying image without the overwrite flag

--- python/tomography/ct_reconstruction.py
+++ python/tomography/ct_reconstruction.py
@@ -77,10 +77,10 @@ if __name__ == '__main__':
             reconstructed_file.close()
             os.remove(reconstructed_name)
             input_file.create_dataset(output_name, data=reconstructed_image)
-            if not args.batch:
-                plt.figure()
-                plt.imshow(reconstructed_image)
-                plt.ion()
-                plt.show()
-                raw_input("Press ENTER to continue.")
             input_file.close()
+        if not args.batch:
+            plt.figure()
+            plt.imshow(reconstructed_image)
+            plt.ion()
+            plt.show()
+            raw_input("Press ENTER to continue.")
