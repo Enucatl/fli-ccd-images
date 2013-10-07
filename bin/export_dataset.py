3dc8ee205fcf28a626f84dfa6a4b01a623063d8b
Matteo Abis <gmatteo..abis@gmail.com>
2013-10-07 10:51:12 +0200

    added bin folder with scripts

---
+++ bin/export_dataset.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+
+"""Export a dataset to an image."""
+
+from __future__ import division, print_function
+
+import matplotlib.pyplot as plt
+import h5py
+
+import readimages_utils.rcparams #pylint: disable=W0611
+from projections.commandline_parser import commandline_parser
+from readimages_utils.hadd import hadd
+
+commandline_parser.description = __doc__
+commandline_parser.add_argument('--dataset', metavar='DATASET',
+        nargs='+', default=['postprocessing/stack_pixel_510'],
+        help='dataset(s) in the HDF5 file to be exported')
+commandline_parser.add_argument('--show', action='store_true',
+        help='show each image.')
+
+if __name__ == '__main__':
+    args = commandline_parser.parse_args()
+    extension = args.format[0]
+    dataset_names = args.dataset
+    file_name = hadd(args.file)
+    input_file = h5py.File(file_name, "r")
+    n = len(dataset_names)
+    print('saving {0} images:'.format(n))
+    for i, name in enumerate(dataset_names):
+        try:
+            image_array = input_file[name]
+        except KeyError:
+            print(name, "not found!\n\n")
+            raise
+        output_name = "{0}.{1}".format(
+                name, extension)
+        without_slashes = output_name.replace("/", "_")
+        full_output_path = file_name.replace(".hdf5", "_" + without_slashes)
+        print(full_output_path)
+        plt.imsave(full_output_path, image_array)
+        if args.show:
+            plt.figure()
+            plt.imshow(image_array)
+            plt.ion()
+            plt.show()
+            raw_input("Press ENTER to continue.")
+    print()
+    print("Done!")
+    print()
---
+++ bin/export_images.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+"""Convert HDF5 images to any image format supported by matplotlib."""
+
+from __future__ import division, print_function
+import os
+import shutil
+
+import matplotlib.pyplot as plt
+from raw_images.base_analyser import BaseHDF5Analyser
+from readimages_utils.hadd import hadd
+
+class ImageConverter(BaseHDF5Analyser):
+    """Convert all raw images in to an image format."""
+    def __init__(self, extension, file_name, *args, **kwargs):
+        self.extension = extension.lower()
+        self.parent_dir, _ = os.path.splitext(hadd(file_name))
+        self.image_dir = os.path.join(self.parent_dir, self.extension)
+        super(ImageConverter,
+                self).__init__(file_name, *args, **kwargs)
+
+    def output_name(self):
+        return self.image_dir
+    
+    def output_exists(self, name):
+        return os.path.exists(name)
+
+    def if_not_exists(self):
+        """Make output folder"""
+        if os.path.exists(self.image_dir):
+            shutil.rmtree(self.image_dir)
+        os.makedirs(self.image_dir)
+        super(ImageConverter, self).if_not_exists()
+
+    def analyse_histogram(self, i, name, hist):
+        super(ImageConverter, self).analyse_histogram(i, hist)
+        plt.imshow(hist,
+                origin='lower',
+                )
+        output_file_name = "{0}.{1}".format(name, extension)
+        plt.imsave(os.path.join(self.output_name(), output_file_name),
+                hist)
+
+
+    def close(self):
+        print()
+        print("created image files in", self.output_name())
+        print()
+
+if __name__ == '__main__':
+    from raw_images.commandline_parser import commandline_parser
+
+    commandline_parser.description = ImageConverter.__doc__
+    commandline_parser.add_argument('--format', metavar='FORMAT',
+            nargs=1, default=["tif"], help='output format')
+    args = commandline_parser.parse_args()
+    extension = args.format[0]
+    overwrite = args.overwrite
+    use_corrected = args.corrected
+    open_option = "r"
+    with ImageConverter(extension,
+            args.file,
+            open_option,
+            use_corrected,
+            overwrite) as analyser:
+        if not analyser.exists_in_file:
+            for i, (name, histogram) in enumerate(analyser.images.iteritems()):
+                analyser.analyse_histogram(i, name, histogram)
+        else:
+            pass
